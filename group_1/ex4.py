f214cfe755e02743301f0382ecb5badb74d5f6a1
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-10-25 16:01:18 +0000

    group_1: ex4: Simplify it

--- group_1/ex4.py
+++ group_1/ex4.py
@@ -1,20 +1,14 @@
 def generated(word, alphabet, sep="_"):
     if sep == "":
-        chars = list(word)
-        for char in chars:
-            if char in alphabet:
-                continue
-            else:
-                return False
-        return True
+        words = list(word)
     else:
         words = word.split(sep)
-        for char in words:
-            if char in alphabet:
-                continue
-            else:
-                return False
-        return True
+
+    for char in words:
+        if char not in alphabet:
+            return False
+    return True
+
 
 word = "a_b_color_b_a"
 result = generated(word, set(["a", "b", "color"]))
